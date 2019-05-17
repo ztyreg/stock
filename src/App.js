6ddc5a14c9b1c933266e3762083b3186dd7bcb2b
y <y@example.com>
2019-05-17 15:17:23 +0800

    add table loading effect

--- src/App.js
+++ src/App.js
@@ -9,53 +9,16 @@ import reqwest from 'reqwest';
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 
-const columns = [
-    {
-        title: 'Match',
-        dataIndex: 'league[0]',
-        key: 'league',
-    },
-    {
-        title: 'Time',
-        dataIndex: 'matchTime',
-        render: (value, record) =>
-        <span title={record.matchYear + "-" + record.matchDate + " " + record.matchTime}>
-            {record.matchDate + " " + record.matchTime}
-        </span>,
-        key: 'matchTime',
-    },
-    {
-        title: 'Home Team',
-        dataIndex: 'home[0]',
-        key: 'home',
-    },
-    {
-        title: 'Score',
-        dataIndex: 'score',
-        render: (value, record) => <span>{record.homeScore} - {record.guestScore}</span>,
-        key: 'score',
-    },
-    {
-        title: 'Guest Team',
-        dataIndex: 'guest[0]',
-        key: 'guest',
-    },
-    {
-        title: 'Half score',
-        dataIndex: 'halfScore',
-        key: 'halfScore',
-        render: (value, record) => <span>{record.homeHalfScore} - {record.guestHalfScore}</span>,
-    }
-];
 
 class App extends Component {
     state = {
         data: [],
         loading: false,
+        lang: 0,
     };
 
     fetch = () => {
-        this.setState({ loading: true });
+        this.setState({loading: true});
         reqwest({
             url: '/worldcup_2018.json',
             method: 'get',
@@ -72,7 +35,48 @@ class App extends Component {
         this.fetch();
     }
 
+    handleLangChange = (e) => {
+        this.setState({
+            lang: e.target.value
+        });
+    };
+
     render() {
+        const columns = [
+            {
+                title: 'Match',
+                dataIndex: 'league',
+                render: league => <span>{league[this.state.lang]}</span>,
+            },
+            {
+                title: 'Time',
+                dataIndex: 'matchTime',
+                render: (value, record) =>
+                    <span title={record.matchYear + "-" + record.matchDate + " " + record.matchTime}>
+                        {record.matchDate + " " + record.matchTime}
+                        </span>,
+            },
+            {
+                title: 'Home Team',
+                dataIndex: 'home',
+                render: home => <span>{home[this.state.lang]}</span>,
+            },
+            {
+                title: 'Score',
+                dataIndex: 'score',
+                render: (value, record) => <span>{record.homeScore} - {record.guestScore}</span>,
+            },
+            {
+                title: 'Guest Team',
+                dataIndex: 'guest',
+                render: guest => <span>{guest[this.state.lang]}</span>,
+            },
+            {
+                title: 'Half score',
+                dataIndex: 'halfScore',
+                render: (value, record) => <span>{record.homeHalfScore} - {record.guestHalfScore}</span>,
+            }
+        ];
         return (
             <div className="App">
                 <nav className="navbar navbar-light bg-light">
@@ -86,20 +90,20 @@ class App extends Component {
                         <Checkbox>Show Red</Checkbox>
                         <Checkbox>Show Yellow</Checkbox>
 
-                        <RadioGroup defaultValue="0">
-                            <RadioButton value="0">field1</RadioButton>
-                            <RadioButton value="1">field2</RadioButton>
-                            <RadioButton value="2">field3</RadioButton>
+                        <RadioGroup defaultValue="0" onChange={this.handleLangChange}>
+                            <RadioButton value={0}>field1</RadioButton>
+                            <RadioButton value={1}>field2</RadioButton>
+                            <RadioButton value={2}>field3</RadioButton>
                         </RadioGroup>
                     </div>
 
                     <Table
-                    dataSource={this.state.data}
-                    columns={columns}
-                    size={"middle"}
-                    pagination={false}
-                    rowKey={record => record.matchId}
-                    loading={this.state.loading}
+                        dataSource={this.state.data}
+                        columns={columns}
+                        size={"middle"}
+                        pagination={false}
+                        rowKey={record => record.matchId}
+                        loading={this.state.loading}
                     />
 
                 </div>
