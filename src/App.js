026d55486952af21b168202447c555bee67f01c6
y <y@example.com>
2019-05-17 13:04:40 +0800

    add table of antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
 import {Checkbox} from 'antd';
 import {Radio} from 'antd';
+import {Table} from 'antd';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
+
+const dataSource = [
+    {
+        key: '1',
+        name: '胡彦斌',
+        age: 32,
+        address: '西湖区湖底公园1号',
+    },
+    {
+        key: '2',
+        name: '胡彦祖',
+        age: 42,
+        address: '西湖区湖底公园1号',
+    },
+];
+
+const columns = [
+    {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
 
 class App extends Component {
     render() {
@@ -18,14 +52,18 @@ class App extends Component {
                 </nav>
 
                 <div className="container mt-3">
-                    <Checkbox>Show Red</Checkbox>
-                    <Checkbox>Show Yellow</Checkbox>
+                    <div className="filter my-3">
+                        <Checkbox>Show Red</Checkbox>
+                        <Checkbox>Show Yellow</Checkbox>
 
-                    <RadioGroup defaultValue="0">
-                        <RadioButton value="0">field1</RadioButton>
-                        <RadioButton value="1">field2</RadioButton>
-                        <RadioButton value="2">field3</RadioButton>
-                    </RadioGroup>
+                        <RadioGroup defaultValue="0">
+                            <RadioButton value="0">field1</RadioButton>
+                            <RadioButton value="1">field2</RadioButton>
+                            <RadioButton value="2">field3</RadioButton>
+                        </RadioGroup>
+                    </div>
+
+                    <Table dataSource={dataSource} columns={columns} size={"middle"} pagination={false}/>
 
                 </div>
 
