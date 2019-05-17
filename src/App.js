4526af440a815d295f6007078f516cd8b55b810d
y <y@example.com>
2019-05-17 12:43:59 +0800

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
+import React, {Component} from 'react';
+import {Button} from 'antd';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Button type="primary">Button</Button>
+                <nav className="navbar navbar-light bg-light">
+                    <div className="container">
+                        <a className="navbar-brand" href="/">Stock Price</a>
+                    </div>
+                </nav>
+
+                <div className="container mt-3">
+                    <Button type="primary">Button</Button>
+                </div>
+
             </div>
         );
     }
