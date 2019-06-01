0e9e3af1b90b48e7ec3d73c232391603e6a693d6
y <y@example.com>
2019-06-01 23:40:41 +0800

    Added multi pages.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React, {Component} from 'react';
-import {Checkbox} from 'antd';
-import {Radio} from 'antd';
-import {Badge} from 'antd';
-import {Table} from 'antd';
-import {Dropdown, Menu, Icon} from 'antd';
-import {message} from 'antd';
-import {Form} from 'antd';
-import {Drawer, Button, Col, Row, Input, Select, DatePicker} from 'antd';
-import {Layout, Breadcrumb} from 'antd';
+import {Checkbox, Table, Menu, Icon, Form, Drawer, Button, Col, Row, Input, Layout, Breadcrumb} from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import reqwest from 'reqwest';
 
 const {SubMenu} = Menu;
 const {Header, Content, Sider} = Layout;
 
-
-const RadioButton = Radio.Button;
-const RadioGroup = Radio.Group;
-
+/**
+ * test if the form input has error
+ * @param fieldsError
+ * @returns {boolean}
+ */
 function hasErrors(fieldsError) {
     return Object.keys(fieldsError).some(field => fieldsError[field]);
 }
 
+/**
+ * the search bar
+ * copied from antd
+ */
 class HorizontalSearchForm extends React.Component {
     componentDidMount() {
         // To disabled submit button at the beginning.
@@ -45,8 +42,8 @@ class HorizontalSearchForm extends React.Component {
         // Only show error after a field is touched.
         const searchError = isFieldTouched('search') && getFieldError('search');
         return (
-            <div className="container" align="right">
-                <Form layout="inline" onSubmit={this.handleSubmit}>
+            <div className="container " align="right" style={{padding: "0"}}>
+                <Form layout="inline" onSubmit={this.handleSubmit} style={{margin: "0"}}>
                     <Form.Item validateStatus={searchError ? 'error' : ''} help={searchError || ''}>
                         {getFieldDecorator('username', {
                             rules: [{required: true, message: 'Please input your username!'}],
@@ -57,7 +54,7 @@ class HorizontalSearchForm extends React.Component {
                             />,
                         )}
                     </Form.Item>
-                    <Form.Item>
+                    <Form.Item style={{margin: "0"}}>
                         <Button type="primary" htmlType="submit" disabled={hasErrors(getFieldsError())}>
                             Search
                         </Button>
@@ -70,6 +67,118 @@ class HorizontalSearchForm extends React.Component {
 
 const WrappedHorizontalSearchForm = Form.create({name: 'horizontal_search'})(HorizontalSearchForm);
 
+/**
+ * the drawer
+ * copied from antd
+ */
+class DrawerForm extends React.Component {
+    state = {
+        visible: false,
+        nametocode: true
+    };
+
+    showDrawer = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    onClose = () => {
+        this.setState({
+            visible: false,
+        });
+    };
+
+    handleNameToCodeChange = () => {
+        this.setState({
+            nametocode: true
+        })
+    };
+
+    handleCodeToNameChange = () => {
+        this.setState({
+            nametocode: false
+        })
+    };
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <div>
+                <Button type="primary" onClick={this.showDrawer}>
+                    <Icon type="right"/> Convert
+                </Button>
+                <Drawer
+                    title="Convert between stock name and code"
+                    width={480}
+                    onClose={this.onClose}
+                    visible={this.state.visible}
+                >
+                    <Form layout="vertical" hideRequiredMark>
+                        {/* divide the drawer into rows and columns */}
+                        <Row gutter={16}>
+                            <Col span={4}>
+                                {/*margin: external whitespace; padding: internal whitespace */}
+                                <div className="container" align="right" style={{margin: "0", padding: "0"}}>
+                                    {/* I did not use a group of radio buttons here since the buttons
+                                    are spatially separated. Instead, I used two checkboxes and a function
+                                    to handle the change. */}
+                                    <Checkbox checked={this.state.nametocode} onChange={this.handleNameToCodeChange}/>
+                                </div>
+                            </Col>
+                            <Col span={16}>
+                                <Form.Item label="Name to code">
+                                    {getFieldDecorator('name', {
+                                        rules: [{required: true, message: 'Please enter stock name'}],
+                                    })(<Input placeholder="Please enter stock name"/>)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row gutter={16}>
+                            <Col span={4}>
+                                <div className="container" align="right" style={{margin: "0", padding: "0"}}>
+                                    <Checkbox checked={!this.state.nametocode} onChange={this.handleCodeToNameChange}/>
+                                </div>
+                            </Col>
+                            <Col span={16}>
+                                <Form.Item label="Code to name">
+                                    {getFieldDecorator('name', {
+                                        rules: [{required: true, message: 'Please enter stock code'}],
+                                    })(<Input placeholder="Please enter stock code"/>)}
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                    </Form>
+                    <div
+                        style={{
+                            position: 'absolute',
+                            left: 0,
+                            bottom: 0,
+                            width: '100%',
+                            borderTop: '1px solid #e9e9e9',
+                            padding: '10px 16px',
+                            background: '#fff',
+                            textAlign: 'right',
+                        }}
+                    >
+                        <Button onClick={this.onClose} style={{marginRight: 8}}>
+                            Cancel
+                        </Button>
+                        <Button onClick={this.onClose} type="primary">
+                            Submit
+                        </Button>
+                    </div>
+                </Drawer>
+            </div>
+        );
+    }
+}
+
+const ConvertForm = Form.create()(DrawerForm);
+
+/**
+ * the table
+ */
 class ResultTable extends Component {
 
     render() {
@@ -104,6 +213,9 @@ class ResultTable extends Component {
                 dataIndex: 'stock_state',
                 render: state => <span>{state}</span>,
             },
+
+            /* the following column contains colored badgers
+            * could be used to color the ups (red) and downs (green) */
             // {
             //     title: 'Home Team',
             //     dataIndex: 'home',
@@ -125,6 +237,10 @@ class ResultTable extends Component {
             //         <span>{home[this.state.lang]}</span>
             //     </div>
             // },
+
+            /* the following column contains simulation buttons
+            * could be used to simulate database change
+            * works in coordinate with handleOperation function in App class */
             // {
             //     title: 'Simulation',
             //     dataIndex: 'operation',
@@ -152,19 +268,6 @@ class ResultTable extends Component {
         return (
             <div className="container mt-3">
 
-                {/*<div className="filter my-3">*/}
-                {/*<Checkbox checked={this.state.showRed} onChange={this.handleShowRedChange}>Show*/}
-                {/*Red</Checkbox>*/}
-                {/*<Checkbox checked={this.state.showYellow} onChange={this.handleShowYellowChange}>Show*/}
-                {/*Yellow</Checkbox>*/}
-
-                {/*<RadioGroup defaultValue="0" onChange={this.handleLangChange}>*/}
-                {/*<RadioButton value={0}>field1</RadioButton>*/}
-                {/*<RadioButton value={1}>field2</RadioButton>*/}
-                {/*<RadioButton value={2}>field3</RadioButton>*/}
-                {/*</RadioGroup>*/}
-                {/*</div>*/}
-
                 <Table
                     dataSource={this.props.data}
                     columns={columns}
@@ -181,55 +284,52 @@ class ResultTable extends Component {
 
 }
 
+class StockInformation extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Layout>
+                    <Sider width={200} style={{background: '#fff'}}>
+                        <div className={"container mt-1"}>
+                            {/** the convert button */}
+                            <ConvertForm/>
+                        </div>
+                    </Sider>
+                    <Layout style={{background: '#fff'}}>
+                        <div className="container">
+                            {/** the search bar*/}
+                            <WrappedHorizontalSearchForm/>
+                        </div>
+                    </Layout>
+                </Layout>
 
-class App extends Component {
-    state = {
-        data: [],
-        loading: false,
-        showRed: true,
-        showYellow: true,
-    };
+                {/**
+                 * data: database data
+                 * loading: whether the data is loading
+                 */}
+                <ResultTable
+                    data={this.props.data}
+                    loading={this.props.loading}
+                />
 
-    fetch = () => {
-        this.setState({loading: true});
-        reqwest({
-            url: '/stock_information.json',
-            method: 'get',
-            type: 'json',
-        }).then(data => {
-            this.setState({
-                loading: false,
-                data: data.results,
-            });
-        });
-    };
-
-    // handleClick = e => {
-    //     console.log('click ', e);
-    // };
-
-    componentDidMount() {
-        this.fetch();
+            </div>
+        )
     }
 
-    // handleLangChange = (e) => {
-    //     this.setState({
-    //         lang: e.target.value
-    //     });
-    // };
+}
 
-    // handleShowRedChange = (e) => {
-    //     this.setState({
-    //         showRed: e.target.checked
-    //     })
-    // };
 
-    // handleShowYellowChange = (e) => {
-    //     this.setState({
-    //         showYellow: e.target.checked
-    //     })
-    // };
+/**
+ * the outmost class
+ * page layout is set in this class
+ * other components are then added into the layout
+ */
+class App extends Component {
 
+
+    /* this function is passed to ResultTable
+     * used to simulate database change
+     */
     // handleOperation = (type, matchId) => {
     //     // message.success('This is a prompt message for success, and it will disappear in 10 seconds', 10);
     //     let foundIndex = this.state.data.findIndex(x => x.matchId === matchId);
@@ -254,10 +354,12 @@ class App extends Component {
     //     }
     // };
 
-
     render() {
-
+        /* just enum in JS */
+        /* the pages */
         return (
+            /* layout */
+            <Router>
             <Layout>
                 <Header className="header">
                     <div className="logo"/>
@@ -267,8 +369,12 @@ class App extends Component {
                         defaultSelectedKeys={['1']}
                         style={{lineHeight: '64px'}}
                     >
-                        <Menu.Item key="1">Panel</Menu.Item>
-                        <Menu.Item key="2">About</Menu.Item>
+                        <Menu.Item key="1">
+                            <Link to={`/panel/stocks/all`}>Panel</Link>
+                        </Menu.Item>
+                        <Menu.Item key="4">
+                            <Link to={`/about`}>About</Link>
+                        </Menu.Item>
                     </Menu>
                 </Header>
                 <Layout>
@@ -281,18 +387,22 @@ class App extends Component {
                         >
                             <SubMenu
                                 key="sub1"
-                                title={<span> <Icon type="search"/>Stocks</span>}
+                                title={<span> <Icon type={"search"}/>Stocks</span>}
                             >
-                                <Menu.Item key="1">Show all</Menu.Item>
-                                <Menu.Item key="2">Convert</Menu.Item>
+                                <Menu.Item key="1">
+                                    <Link to={`/panel/stocks/all`}>Show all</Link>
+                                </Menu.Item>
                             </SubMenu>
                             <SubMenu
                                 key="sub2"
                                 title={<span> <Icon type="user"/>Account</span>}
                             >
-                                <Menu.Item key="3">Information</Menu.Item>
-                                <Menu.Item key="4">Upgrade</Menu.Item>
-                                <Menu.Item key="5">Sign out</Menu.Item>
+                                <Menu.Item key="2">
+                                    <Link to={`/panel/account/info`}>Information</Link>
+                                </Menu.Item>
+                                <Menu.Item key="3">
+                                    <Link to={`/panel/account/upgrade`}>Upgrade</Link>
+                                </Menu.Item>
                             </SubMenu>
                         </Menu>
                     </Sider>
@@ -300,7 +410,7 @@ class App extends Component {
                         <Breadcrumb style={{margin: '16px 0'}}>
                             <Breadcrumb.Item>Panel</Breadcrumb.Item>
                             <Breadcrumb.Item>Stocks</Breadcrumb.Item>
-                            <Breadcrumb.Item>Information</Breadcrumb.Item>
+                            <Breadcrumb.Item>Show all</Breadcrumb.Item>
                         </Breadcrumb>
                         <Content
                             style={{
@@ -310,24 +420,111 @@ class App extends Component {
                                 minHeight: 280,
                             }}
                         >
-                            <div className="App">
-
-                                <WrappedHorizontalSearchForm/>
-
-                                <ResultTable
-                                    data={this.state.data}
-                                    loading={this.state.loading}
-                                />
-
-
-                            </div>
+                                <Route exact path="/" component={showPanelStockInformation} />
+                                <Route exact path="/panel/stocks/all"  component={showPanelStockInformation} />
+                                <Route exact path="/panel/account/info" component={showPanelAccountInformation} />
+                                <Route exact path="/panel/account/upgrade" component={showPanelAccountUpgrade} />
+                                <Route exact path="/about" component={showAbout} />
                         </Content>
                     </Layout>
                 </Layout>
             </Layout>
+            </Router>
 
         );
     }
 }
+
+class PanelStockInformation extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        data: [],
+        loading: false
+    };
+
+    /**
+     * fetch JSON data
+     */
+    fetch = () => {
+        this.setState({loading: true});
+        reqwest({
+            url: '/stock_data.json',
+            method: 'get',
+            type: 'json',
+        }).then(data => {
+            this.setState({
+                loading: false,
+                data: data.results,
+            });
+        });
+    };
+
+    /* this function could be used write click events to console */
+    // handleClick = e => {
+    //     console.log('click ', e);
+    // };
+
+    componentDidMount() {
+        this.fetch();
+    }
+
+    render() {
+        return (
+            <StockInformation
+                data={this.state.data}
+                loading={this.state.loading}
+            />
+        );
+    }
+}
+
+function showPanelStockInformation() { return new PanelStockInformation(); }
+
+//TODO
+class PanelAccountInformation extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+function showPanelAccountInformation() { return new PanelAccountInformation(); }
+
+class PanelAccountUpgrade extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+function showPanelAccountUpgrade() { return new PanelAccountUpgrade(); }
+
+class About extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+function showAbout() { return new About(); }
+
+//TODO
 
 export default App;
