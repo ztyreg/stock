e102b6b5bfb8c0dfce07e1b5577b71092b04d4f5
Frostland12138 <xuhongjia902@126.com>
2019-06-14 02:50:35 -0400

    add Kline and dealinfo,need debug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {Table, Menu, Icon, Button, Input, Layout, Breadcrumb, Modal} from 'antd';
+import {Table, Menu, Icon, Button, Input, Layout, Breadcrumb, Modal,Card} from 'antd';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Highlighter from 'react-highlight-words';
-
+import ReactEcharts from 'echarts-for-react';
 import './App.css';
 import reqwest from 'reqwest';
 
@@ -123,6 +123,120 @@ class ResultTable extends Component {
         return <Table columns={columns} dataSource={this.props.data}/>;
     }
 }
+class ResultTable_deal extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    state = {
+        searchText: '',
+    };
+
+    getColumnSearchProps = dataIndex => ({
+        filterDropdown: ({setSelectedKeys, selectedKeys, confirm, clearFilters}) => (
+            <div style={{padding: 8}}>
+                <Input
+                    ref={node => {
+                        this.searchInput = node;
+                    }}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+                    style={{width: 188, marginBottom: 8, display: 'block'}}
+                />
+                <Button
+                    type="primary"
+                    onClick={() => this.handleSearch(selectedKeys, confirm)}
+                    icon="search"
+                    size="small"
+                    style={{width: 90, marginRight: 8}}
+                >
+                    Search
+                </Button>
+                <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{width: 90}}>
+                    Reset
+                </Button>
+            </div>
+        ),
+        filterIcon: filtered => (
+            <Icon type="search" style={{color: filtered ? '#1890ff' : undefined}}/>
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                .toString()
+                .toLowerCase()
+                .includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: visible => {
+            if (visible) {
+                setTimeout(() => this.searchInput.select());
+            }
+        },
+        render: text => (
+            <Highlighter
+                highlightStyle={{backgroundColor: '#ffc069', padding: 0}}
+                searchWords={[this.state.searchText]}
+                autoEscape
+                textToHighlight={text.toString()}
+            />
+        ),
+    });
+
+    handleSearch = (selectedKeys, confirm) => {
+        confirm();
+        this.setState({searchText: selectedKeys[0]});
+    };
+
+    handleReset = clearFilters => {
+        clearFilters();
+        this.setState({searchText: ''});
+    };
+
+    render() {
+        const columns = [
+            {
+                title: 'Key',
+                dataIndex: 'Key',
+                key: 'Key',
+                width: '20%',
+                ...this.getColumnSearchProps('Key'),
+            },
+            {
+                title: 'SellOrBuy',
+                dataIndex: 'SellOrBuy',
+                key: 'SellOrBuy',
+                width: '20%',
+                ...this.getColumnSearchProps('SellOrBuy'),
+            },
+            {
+                title: 'stockID',
+                dataIndex: 'stockID',
+                key: 'stockID',
+                width: '15%',
+            },
+            {
+                title: 'number',
+                dataIndex: 'number',
+                key: 'number',
+                width: '15%',
+            },
+            {
+                title: 'time',
+                dataIndex: 'time',
+                key: 'time',
+                width: '15%',
+            },
+            {
+                title: 'states',
+                dataIndex: 'states',
+                key: 'states',
+                width: '15%',
+                ...this.getColumnSearchProps('states'),
+            },
+        ];
+        return <Table columns={columns} dataSource={this.props.data}/>;
+    }
+}
 
 class StockHelp extends Component {
     state = {visible: false};
@@ -194,13 +308,13 @@ class PanelStockInformation extends Component {
     fetch = () => {
         this.setState({loading: true});
         reqwest({
-            url: '/stock_data.json',
+            url: '/st_new.json',
             method: 'get',
             type: 'json',
         }).then(data => {
             this.setState({
                 loading: false,
-                data: data.results,
+                data: data.stocks,
             });
         });
     };
@@ -228,7 +342,56 @@ class PanelStockInformation extends Component {
 function showPanelStockInformation() {
     return new PanelStockInformation();
 }
+class PanelStockdealInformation extends Component {
+    constructor(props) {
+        super(props);
+    }
 
+    state = {
+        data: [],
+        loading: false
+    };
+
+    /**
+     * fetch JSON data
+     */
+    fetch = () => {
+        this.setState({loading: true});
+        reqwest({
+            url: '/deal.json',
+            method: 'get',
+            type: 'json',
+        }).then(data => {
+            this.setState({
+                loading: false,
+                data: data.results,
+            });
+        });
+    };
+
+    componentDidMount() {
+        this.fetch();
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="Instruction">
+                    <StockHelp/>
+                </div>
+                <div className="Table_deal" style={{paddingTop: '15px'}}>
+                    <ResultTable_deal
+                        {...this.state}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+function showPanelStockdealInformation() {
+    return new PanelStockdealInformation();
+}
 class PanelAccountInformation extends Component {
     constructor(props) {
         super(props);
@@ -278,6 +441,75 @@ class About extends Component {
 
 function showAbout() {
     return new About();
+}
+function splitData(rawData) {
+    var categoryData = [];
+    var values = []
+    for (var i = 0; i < rawData.length; i++) {
+        categoryData.push(rawData[i].splice(0, 1)[0]);
+        values.push(rawData[i])
+    }
+    return {
+        categoryData: categoryData,
+        values: values
+    };
+}
+class Kline extends Component {
+    constructor(props) {
+        super(props);
+    }
+    data0: [[ 2320.26,2320.26,2287.3,2362.94],
+    [ 2300,2291.3,2288.26,2308.38],
+    [ 2295.35,2346.5,2295.35,2346.92],
+    [ 2347.22,2358.98,2337.35,2363.8],
+    [ 2360.75,2382.48,2347.89,2383.76],
+    [ 2383.43,2385.42,2371.23,2391.82],
+    [ 2377.41,2419.02,2369.57,2421.15]
+    ]
+    data1:['周一','周二','周三','周四','周五','周六','周日']
+    getOption =()=> {
+    let option = {
+      title:{
+        text:'大盘',
+        x:'center'
+      },
+      xAxis: {
+        data: ['周一','周二','周三','周四','周五','周六','周日']
+    },
+    yAxis: {
+	min:2200
+
+	},
+
+    series: [{
+        type: 'k',
+        data: [[ 2320.26,2320.26,2287.3,2362.94],
+    [ 2300,2291.3,2288.26,2308.38],
+    [ 2295.35,2346.5,2295.35,2346.92],
+    [ 2347.22,2358.98,2337.35,2363.8],
+    [ 2360.75,2382.48,2347.89,2383.76],
+    [ 2383.43,2385.42,2371.23,2391.82],
+    [ 2377.41,2419.02,2369.57,2421.15]
+    ]
+    }]
+    }
+   return option
+  }
+
+  render(){
+    return(
+      <div>
+        <Card title="K线图">
+            <ReactEcharts option={this.getOption()} theme="Imooc"  style={{height:'400px'}}/>
+        </Card>
+
+      </div>
+    )
+  }
+}
+
+function showKline() {
+    return new Kline();
 }
 
 class App extends Component {
@@ -348,6 +580,13 @@ class App extends Component {
                                             <Menu.Item key="1">
                                                 <Link to={`/panel/stocks/all`}>All</Link>
                                             </Menu.Item>
+					    <Menu.Item key="6">
+                                                <Link to={`/panel/stocks/Kline`}>Kline</Link>
+                                            </Menu.Item>
+					    <Menu.Item key="5">
+                                                <Link to={`/panel/stocks/deal`}>Deal</Link>
+                                            </Menu.Item>
+					    
                                         </SubMenu>
                                         <SubMenu
                                             key="sub2"
@@ -393,6 +632,24 @@ class App extends Component {
                                     </Breadcrumb>
                                 )
                             }}/>
+			    <Route exact path="/panel/stocks/deal" component={() => {
+                                return (
+                                    <Breadcrumb style={{margin: '16px 0'}}>
+                                        <Breadcrumb.Item>Panel</Breadcrumb.Item>
+                                        <Breadcrumb.Item>Stocks</Breadcrumb.Item>
+                                        <Breadcrumb.Item>Deal</Breadcrumb.Item>
+                                    </Breadcrumb>
+                                )
+                            }}/>
+			    <Route exact path="/panel/stocks/Kline" component={() => {
+                                return (
+                                    <Breadcrumb style={{margin: '16px 0'}}>
+                                        <Breadcrumb.Item>Panel</Breadcrumb.Item>
+                                        <Breadcrumb.Item>Stocks</Breadcrumb.Item>
+                                        <Breadcrumb.Item>Kline</Breadcrumb.Item>
+                                    </Breadcrumb>
+                                )
+                            }}/>
                             <Route exact path="/panel/account/info" component={() => {
                                 return (
                                     <Breadcrumb style={{margin: '16px 0'}}>
@@ -429,6 +686,8 @@ class App extends Component {
                             >
                                 <Route exact path="/" component={showPanelStockInformation}/>
                                 <Route exact path="/panel/stocks/all" component={showPanelStockInformation}/>
+				<Route exact path="/panel/stocks/deal" component={showPanelStockdealInformation}/>
+				<Route exact path="/panel/stocks/Kline" component={showKline}/>
                                 <Route exact path="/panel/account/info" component={showPanelAccountInformation}/>
                                 <Route exact path="/panel/account/upgrade" component={showPanelAccountUpgrade}/>
                                 <Route exact path="/about" component={showAbout}/>
